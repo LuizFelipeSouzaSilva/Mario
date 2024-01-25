645612ee6a9d7baf93ad239401443951f6e43085
Luiz Felipe <luiz.felipesouza0306@gmail.com>
2024-01-25 12:20:36 -0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+let mario = document.querySelector('.mario')
+let pipe = document.querySelector('.pipe')
+
+let jump = () => {
+    mario.classList.add('jump')
+
+    setTimeout(() =>{
+        mario.classList.remove('jump')
+    }, 500)
+}
+
+let loop = setInterval(() =>{
+
+    console.log('loop')
+
+    let pipePosition = pipe.offsetLeft;
+    let marioPosition = window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = "./assets/images/game-over.png";
+        mario.style.width ='65px'
+        mario.style.marginLeft ='50px'
+
+        clearInterval(loop)
+    }
+}, 10)
+
+document.addEventListener('keydown', jump)
+
